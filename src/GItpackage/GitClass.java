b79d5e43e5f49ff1aa682d3173dfd76461e2bd80
kklab <kklab@131.206.23.171>
2015-09-29 14:52:17 +0900

    コンストラクタ作成

---
+++ src/GItpackage/GitClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GItpackage;
+
+/**
+ *
+ * @author kklab
+ */
+public class GitClass {
+    private int num;
+    public GitClass(){}
+    public int getNum(){
+    
+    return num;
+    }
+    public  void setNum(int num){
+    
+        this.num = num;
+    
+    }
+}
